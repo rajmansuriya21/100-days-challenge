562215bb6eda0e5dbfdb21e03c6cd8f2e7291a41
Raj Mansuriya <84504923+rajmansuriya21@users.noreply.github.com>
2024-01-18 11:29:43 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (88.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,18 +2,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int ways = 1;
-
-        for (int i = 1; i <= n / 2; i++) {
-            double sum = 1;
-
-            for (int j = i; j < 2 * i; j++) {
-                sum *= (double)(n - j) / (j - i + 1);
+        int ways=1;
+        
+        for(int i=1;i<=n/2;i++){
+            double sum =1;
+            
+            for(int j=i;j< 2*i;j++){
+                sum*=(double)(n-j)/(j-i+1);
             }
-
-            ways +=sum;
+            ways+=sum;
         }
-
         return ways;
     }
 };
